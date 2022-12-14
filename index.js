2e71408fbc48516b9b07517aac202db252f5e4a2
Boyan Velikov <boyanvelikov@gmail.com>
2022-12-14 08:48:44 +0200

    final

--- index.js
+++ index.js
@@ -8,13 +8,11 @@ const alpaca = new Alpaca({
   paper: true
 });
 
-// console.log(alpaca);
-
 let sma20, sma50;
 let lastOrder = 'SELL';
 
 async function initializeAverages() {
-  const initialData = await alpaca.getBars (
+  const initialData = await alpaca.getBars(
     '1Min',
     'SPY',
     {
@@ -35,36 +33,44 @@ async function initializeAverages() {
 initializeAverages();
 
 const client = alpaca.websocket;
+
 client.onConnect(() => {
-    client.subscribe(['AM.SPY']);
-    setTimeout(() => client.disconnect(), 6000*1000);
+  client.subscribe(['AM.SPY']);
+  setTimeout(() => client.disconnect(), 6000*1000);
 });
 
-client.onStockAggMin(((subject, data) => {
-    const nextValue = JSON.parse(data)[0].c;
+client.onStockAggMin((subject, data) => {
+  const nextValue = JSON.parse(data)[0].c;
 
-    const next20 = sma20.nextValue(nextValue);
-    const next50 = sma50.nextValue(nextValue); 
+  const next20 = sma20.nextValue(nextValue);
+  const next50 = sma50.nextValue(nextValue);
 
-    // console.log(`next20: ${next20}`);
-    // console.log(`next50: ${next50}`);
+  console.log(`next20: ${next20}`);
+  console.log(`next50: ${next50}`);
 
-    if(next20 > next50 && lastOrder !== 'BUY') {
-        alpaca.createOrder({
-            symbol: 'SPY',
-            qty: 300,
-            side: 'buy',
-            type: 'market',
-            time_in_force: 'day'
-        });
-        lastOrder = 'BUY';
-        console.log('\nBUY\n');
-    } else if (next20 < next50 && lastOrder !== 'SELL') {
-        //SELL
-    }
+  if (next20 > next50 && lastOrder !== 'BUY') {
+    alpaca.createOrder({
+      symbol: 'SPY',
+      qty: 300,
+      side: 'buy',
+      type: 'market',
+      time_in_force: 'day'
+    });
 
-}));
+    lastOrder = 'BUY';
+    console.log('\nBUY\n');
+  } else if (next20 < next50 && lastOrder !== 'SELL') {
+    alpaca.createOrder({
+      symbol: 'SPY',
+      qty: 300,
+      side: 'sell',
+      type: 'market',
+      time_in_force: 'day'
+    });
 
+    lastOrder = 'SELL';
+    console.log('\nSELL\n');
+  }
+});
 
-
-
+client.connect();
